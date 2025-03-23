fd943da502753e2877825baa67230a2c49dbf996
alenior <alenior@gmail.com>
2025-03-23 10:46:22 -0300

    Primeira versão de teste.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
 android {
     namespace = "com.example.gestao_do_meu_negocio"
     compileSdk = 35
-    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -24,7 +23,7 @@ android {
         applicationId = "com.example.gestao_do_meu_negocio"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 19
+        minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
